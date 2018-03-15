97fddaaac11d9dce0d9a02e308f08f6dc0ee2cb1
NoelleStegner <NoelleStegner@users.noreply.github.com>
2018-03-15 13:15:20 -0700

    noelle-stegner-project2

---
+++ sketch.js
@@ -0,0 +1,432 @@
+const CANVAS_WIDTH = 1105;
+const CANVAS_HEIGHT= 1403;
+function setup(){
+
+  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  background(200)
+
+}
+
+function draw(){
+  colorMode(HSB);
+strokeWeight(4);
+stroke(255, 204, 100);
+
+  fill('#fae');
+  beginShape()
+  vertex(387, 271);
+  vertex(894, 641);
+  vertex(584, 485);
+  vertex(535, 409);
+  vertex(407, 295);
+  endShape(CLOSE)
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(387, 271);
+  vertex(646, 401);
+  vertex(796, 497);
+  vertex(894, 641);
+  endShape(CLOSE);
+
+  fill('#fae');
+  beginShape()
+  vertex(760, 795);
+  vertex(638, 563);
+  vertex(640, 561);
+  vertex(640, 559);
+  vertex(632, 549);
+  vertex(618, 531);
+  vertex(620, 515);
+  vertex(614, 505);
+  vertex(746, 569);
+  endShape(CLOSE);
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(760, 795);
+  vertex(746, 569);
+  vertex(894, 641);
+  endShape(CLOSE);
+
+  fill('#fae')
+  beginShape()
+  vertex(760, 795);
+  vertex(894, 641);
+  vertex(900, 651);
+  vertex(910, 673);
+  endShape(CLOSE);
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(910, 671);
+  vertex(934, 663);
+  vertex(994, 653);
+  vertex(1034, 667);
+  vertex(1050, 695);
+  vertex(760, 795);
+  endShape(CLOSE);
+
+  fill('#fae');
+  beginShape()
+  vertex(760, 795);
+  vertex(1050, 695);
+  vertex(1080, 711);
+  vertex(1078, 717);
+  vertex(1052, 733);
+  vertex(996, 811);
+  vertex(904, 875);
+  vertex(860, 895);
+  endShape(CLOSE);
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(760, 795);
+  vertex(860,895);
+  vertex(850, 921);
+  vertex(814, 951);
+  vertex(792, 963);
+  vertex(750, 977);
+  vertex(726, 991);
+  endShape(CLOSE);
+
+  fill('#fae');
+  beginShape()
+  vertex(760, 795);
+  vertex(726, 991);
+  vertex(553, 1015);
+  endShape(CLOSE);
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(760, 795);
+  vertex(557, 1017);
+  vertex(215, 1103);
+  vertex(175, 1119);
+  vertex(167, 1115);
+  vertex(185, 1095);
+  vertex(265, 1047);
+  vertex(291, 1031);
+  vertex(299, 1015);
+  endShape(CLOSE);
+
+  fill('#fae');
+  beginShape()
+  vertex(760, 795);
+  vertex(299, 1015);
+  vertex(423, 957);
+  vertex(433, 939);
+  vertex(465, 929);
+  vertex(465, 917);
+  vertex(475, 905);
+  vertex(473, 875);
+  vertex(497, 859);
+  vertex(499, 835);
+  vertex(519, 819);
+  vertex(543, 813);
+  endShape(CLOSE)
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(760, 795);
+  vertex(543, 813);
+  vertex(489, 787);
+  vertex(481, 779);
+  vertex(413, 769);
+  vertex(339, 767);
+  vertex(205, 769);
+  vertex(143, 765);
+  vertex(49, 761);
+  vertex(31, 751);
+  vertex(51, 745);
+  vertex(99, 735);
+  vertex(119, 731);
+  vertex(167, 729);
+  vertex(177, 719);
+  vertex(209, 715);
+  vertex(287, 693);
+  vertex(303, 693);
+  vertex(311, 683);
+  vertex(331, 681);
+  endShape(CLOSE);
+
+  fill('#fae');
+  beginShape()
+  vertex(760, 795);
+  vertex(331, 681);
+  vertex(331, 657);
+  vertex(345, 647);
+  vertex(323, 647);
+  vertex(317, 603);
+  vertex(291, 579);
+  vertex(289, 565);
+  vertex(271, 549);
+  vertex(259, 529);
+  vertex(255, 519);
+  vertex(229, 495);
+  vertex(213, 479);
+  vertex(217, 473);
+  vertex(237, 479);
+  vertex(341, 561);
+  vertex(371, 583);
+  vertex(401, 609);
+  vertex(437, 639);
+  vertex(471, 655);
+  vertex(517, 667);
+  vertex(564, 663);
+  endShape(CLOSE);
+
+  fill('rgba(100%,0%,100%,0.5)');
+  beginShape()
+  vertex(760, 795);
+  vertex(564, 663);
+  vertex(588, 651);
+  vertex(588, 641);
+  vertex(610, 631);
+  vertex(612, 613);
+  vertex(636, 605);
+  vertex(666, 605)
+  endShape(CLOSE);
+
+
+  colorMode(HSB);
+  fill(255, 204, 100);
+  beginShape()
+
+  point(384, 272);
+  line(384, 272, 539, 341);
+
+  point(539, 341);
+  line(539, 341, 685, 421);
+
+  point(685, 421);
+  line(685, 421, 802, 501);
+
+  point(802, 501);
+  line(802, 501, 907, 665);
+
+  point(907, 665);
+  line(907, 665, 992, 652);
+
+  point(992, 652);
+  line(992, 652, 1034, 666);
+
+  point(1034, 666);
+  line(1034, 666, 1052, 694);
+
+  point(1052, 694);
+  line(1052, 694, 1080, 708);
+
+  point(1080, 708);
+  line(1080, 708, 1055, 729);
+
+  point(1055, 729);
+  line(1055, 729, 983, 824);
+
+  point(983, 824);
+  line(983, 824, 863, 898);
+
+  point(863, 898);
+  line(863, 898, 851, 920);
+
+  point(851, 920);
+  line(851, 920, 807, 954);
+
+  point(807, 954);
+  line(807, 954, 747, 978);
+
+  point(747, 978);
+  line(747, 978, 731, 988);
+
+  point(731, 988);
+  line(731, 988, 517, 1023);
+
+  point(517, 1023);
+  line(517, 1023, 211, 1102);
+
+  point(211, 1102);
+  line(211, 1102, 171, 1118);
+
+  point(171, 1118);
+  line(171, 1118, 180, 1098);
+
+  point(180, 1098);
+  line(180, 1098, 288, 1032);
+
+  point(288, 1032);
+  line(288, 1032, 309, 1009);
+
+  point(309, 1009);
+  line(309, 1009, 398, 971);
+
+  point(398, 971);
+  line(398, 971, 411, 960);
+
+  point(411, 960);
+  line(411, 960, 422, 956);
+
+  point(422, 956);
+  line(422, 956, 434, 938);
+
+  point(434, 938);
+  line(434, 938, 466, 927);
+
+  point(466, 927);
+  line(466, 927, 466, 910);
+
+  point(466, 910);
+  line(466, 910, 473, 900);
+
+  point(473, 900);
+  line(473, 900, 466, 912);
+
+  point(466, 912);
+  line(466, 912, 473, 902);
+
+  point(473, 902);
+  line(473, 902, 472, 873);
+
+  point(472, 873);
+  line(472, 873, 497, 857);
+
+  point(497, 857);
+  line(497, 857, 500, 834);
+
+  point(500, 834);
+  line(500, 834, 541, 814);
+
+  point(541, 814);
+  line(541, 814, 481, 779);
+
+  point(481, 779);
+  line(481, 779, 424, 771);
+
+  point(424, 771);
+  line(424, 771, 121, 765);
+
+  point(121, 765);
+  line(121, 765, 31, 750);
+
+  point(31, 750);
+  line(31, 750, 107, 732);
+
+  point(107, 732);
+  line(107, 732, 168, 727);
+
+  point(168, 727);
+  line(168, 727, 176, 717);
+
+  point(176, 717);
+  line(176, 717, 203, 714);
+
+  point(203, 714);
+  line(203, 714, 289, 691);
+
+  point(289, 691);
+  line(289, 691, 312, 682);
+
+  point(312, 682);
+  line(312, 682, 329, 679);
+
+  point(329, 679);
+  line(329, 679, 333, 650);
+
+  point(333, 650);
+  line(333, 650, 344, 647);
+
+  point(344, 647);
+  line(344, 647, 321, 623);
+
+  point(321, 623);
+  line(321, 623, 316, 604);
+
+  point(316, 604);
+  line(316, 604, 287, 572);
+
+  point(287, 572);
+  line(287, 572, 296, 562);
+
+  point(296, 562);
+  line(296, 562, 268, 544);
+
+  point(268, 544);
+  line(268, 544, 252, 516);
+
+  point(252, 516);
+  line(252, 516, 213, 473);
+
+  point(213, 473);
+  line(213, 473, 226, 472);
+
+  point(226, 472);
+  line(226, 472, 260, 501);
+
+  point(260, 501);
+  line(260, 501, 360, 574);
+
+  point(360, 574);
+  line(360, 574, 442, 639);
+
+  point(442, 639);
+  line(442, 639, 526, 666);
+
+  point(526, 666);
+  line(526, 666, 589, 650);
+
+  point(589, 650);
+  line(589, 650, 590, 636);
+
+  point(590, 636);
+  line(590, 636, 610, 630);
+
+  point(610, 630);
+  line(610, 630, 613, 614);
+
+  point(613, 614);
+  line(613, 614, 663, 603);
+
+  point(663, 603);
+  line(663, 603, 642, 576);
+
+  point(642, 576);
+  line(642, 576, 639, 558);
+
+  point(639, 558);
+  line(639, 558, 620, 529);
+
+  point(620, 529);
+  line(620, 529, 620, 512);
+
+  point(620, 512);
+  line(620, 512, 580, 478);
+
+  point(580, 478);
+  line(580, 478, 558, 446);
+
+  point(558, 446);
+  line(558, 446, 543, 427);
+
+  point(543, 427);
+  line(543, 427, 532, 411);
+
+  point(532, 411);
+  line(532, 411, 472, 360);
+
+  point(472, 360);
+  line(472, 360, 451, 335);
+
+  point(451, 335);
+  line(451, 335, 413, 300);
+
+  point(413, 300);
+  line(413, 300, 403, 297);
+
+  point(403, 297);
+  line(403, 297, 386, 283);
+
+  point(386, 283);
+  line(386, 283, 384, 272);
+
+  endShape(CLOSE);
+
+}
